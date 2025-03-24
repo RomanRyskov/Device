800873cfcbad7dea4fdd7e04ef4d187bbd8f89a6
Roman_Ryskov <rryskov1111@gmail.com>
2025-03-24 13:47:16 +0300

    задача лифт

---
+++ src/Elevator/Elevator.java
@@ -0,0 +1,39 @@
+package Elevator;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Elevator {
+    private final int maxCapacity = 6;
+    Queue<String> elevator = new LinkedList<>();
+
+    public void enter(String person) {
+        if (isFull()) {
+            System.out.println("Нету места, лифт полный");
+        } else {
+            elevator.offer(person);
+            System.out.println(person + " вошел в лифт.");
+        }
+    }
+
+    public String exit() {
+        if (elevator.isEmpty()) {
+            return "лифт пустой";
+        } else {
+            return elevator.poll();
+        }
+    }
+    public int getCurrentPassenger(){
+        return elevator.size();
+    }
+    public String peekNextToExit(){
+        if (elevator.isEmpty()) {
+            return "Лифт пустой";
+        }
+        return elevator.peek();
+    }
+    public boolean isFull(){
+        return elevator.size() >= maxCapacity;
+    }
+
+}
---
+++ src/Elevator/Main.java
@@ -0,0 +1,17 @@
+package Elevator;
+
+public class Main {
+    public static void main(String[] args) {
+        Elevator elevator = new Elevator();
+        elevator.enter("Дима");
+        elevator.enter("Саша");
+        elevator.enter("Валера");
+        elevator.enter("Никита");
+        elevator.enter("Маша");
+        System.out.println("Из лифта выходит: " + elevator.exit());
+        System.out.println("Следующий из лифта выходит: " + elevator.peekNextToExit());
+        System.out.println("Сейчас в лифте "+elevator.getCurrentPassenger());
+        elevator.exit();
+
+    }
+}
