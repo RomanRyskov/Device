a34f7a4769005b76256634b5b39d3b733e3ad65a
van-lavrik <slavik-van-lavrik@mail.ru>
2025-04-24 20:12:22 +0300

    Stream

--- src/L21_04_2025/Main.java
+++ src/L21_04_2025/Main.java
@@ -2,6 +2,7 @@ package L21_04_2025;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
@@ -17,6 +18,17 @@ public class Main {
                 new Dish("prawns", false, 300, Type.FISH),
                 new Dish("salmon", false, 450, Type.FISH),
                 new Dish("salmon", false, 420, Type.FISH));
+
+        List<Dish> menu2 = Arrays.asList(
+                new Dish("pork2", false, 800, Type.MEAT),
+                new Dish("beef2", false, 700, Type.MEAT),
+                new Dish("chicken2", false, 750, Type.MEAT),
+                new Dish("rice2", true, 120, Type.OTHER),
+                new Dish("season fruit2", true, 800, Type.MEAT),
+                new Dish("pizza2", true, 550, Type.OTHER),
+                new Dish("prawns2", false, 100, Type.FISH),
+                new Dish("salmon2", false, 430, Type.FISH),
+                new Dish("salmon2", false, 420, Type.FISH));
 
         List<String> collect = menu.stream()
                 .filter(dish -> dish.getCalories() < 400)
@@ -50,5 +62,89 @@ public class Main {
         int i = menu.stream().mapToInt(Dish::getCalories).max().orElse(1);
         Map<Type, List<Dish>> collect1 = menu.stream().collect(Collectors.groupingBy(Dish::getType));
         System.out.println(collect1);
+
+        List<List<Dish>> listList = List.of(menu, menu2);
+        List<Dish> collect2 = listList.stream()
+                .map(Collection::stream)
+                .flatMap(Stream::distinct)
+                .collect(Collectors.toList());
+        List<Dish> collect3 = listList.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+        int sum1 = Arrays.stream(nums).sum();
+
+        Dish nullDish = null;
+        Stream.of("home", "java", "dish", "javascript")
+                .map(String::toUpperCase).forEach(System.out::println);
+        Stream.ofNullable(nullDish)
+                .map(Dish::getCalories)
+                .forEach(System.out::println);
+
+        Stream.iterate(0, n -> n + 2).limit(10).forEach(System.out::println);
+        Stream.iterate(0, n -> n < 100, n -> n + 4)
+                .forEach(System.out::println);
+
+        Stream.generate(Math::random)
+                .limit(5)
+                .forEach(System.out::println);
+
+        String strDishName = menu.stream()
+                .map(Dish::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(strDishName);
+
+        menu.stream()
+                .map(Dish::getName)
+                .collect(Collectors.counting());
+
+        Optional<Dish> collect4 = menu.stream()
+                .collect(Collectors.maxBy(Comparator.comparingInt(Dish::getCalories)));
+
+        Integer collect5 = menu.stream()
+                .collect(Collectors.summingInt(Dish::getCalories));
+
+        Double collect6 = menu.stream()
+                .collect(Collectors.averagingInt(Dish::getCalories));
+
+        IntSummaryStatistics collect7 = menu.stream()
+                .collect(Collectors.summarizingInt(Dish::getCalories));
+        System.out.println(collect7);
+
+        int sum2 = menu.stream()
+                .mapToInt(Dish::getCalories)
+                .sum();
+
+        Map<String, List<Dish>> collect8 = menu.stream()
+                .collect(Collectors.groupingBy((Dish::getName)));
+
+        Map<String, List<Dish>> collect9 = menu.stream().collect(Collectors.groupingBy(dish -> {
+            if (dish.getCalories() <= 400) {
+                return "Диета";
+            } else if (dish.getCalories() <= 700) {
+                return "Норма";
+            } else {
+                return "ТОЛСТЫй";
+            }
+        }));
+
+        Map<Type, List<Dish>> collect10 = menu.stream()
+                .filter(dish -> dish.getCalories() > 500)
+                .collect(Collectors.groupingBy(Dish::getType));
+        System.out.println(collect10);
+
+        Map<Type, List<Dish>> collect11 = menu.stream()
+                .collect(Collectors.groupingBy(Dish::getType, Collectors.filtering(dish -> dish.getCalories() > 500, Collectors.toList())));
+        System.out.println(collect11);
+
+        Map<Type, Long> collect12 = menu.stream()
+                .collect(Collectors.groupingBy(Dish::getType, Collectors.counting()));
+        System.out.println(collect12);
+
+        Map<Type, Dish> mapDish = menu.stream()
+                .collect(Collectors.groupingBy(Dish::getType, Collectors.collectingAndThen(
+                        Collectors.maxBy(Comparator.comparingInt(Dish::getCalories)),
+                        Optional::get)));
     }
 }
--- src/grocery_stream/manager/GroceryManager.java
+++ src/grocery_stream/manager/GroceryManager.java
@@ -17,58 +17,45 @@ public class GroceryManager {
     }
 
     public List<GroceryItem> dairyFilter() {
-        return groceryItems.stream()
-                .filter(groceryItem -> groceryItem.getCategory() == Category.DAIRY)
-                .collect(Collectors.toList());
+        return groceryItems.stream().filter(groceryItem -> groceryItem.getCategory() == Category.DAIRY).collect(Collectors.toList());
     }
 
     public List<String> perishableFilter() {
-        return groceryItems.stream()
-                .filter(GroceryItem::isPerishable)
-                .map(GroceryItem::getName)
-                .collect(Collectors.toList());
+        return groceryItems.stream().filter(GroceryItem::isPerishable).map(GroceryItem::getName).collect(Collectors.toList());
     }
+
     public List<GroceryItem> sortByPrice() {
-        return groceryItems.stream()
-                .sorted(Comparator.comparing(GroceryItem::getPrice))
-                .collect(Collectors.toList());
+        return groceryItems.stream().sorted(Comparator.comparing(GroceryItem::getPrice)).collect(Collectors.toList());
     }
+
     public List<GroceryItem> sortByPrice2() {
-        return groceryItems.stream()
-                .sorted(Comparator.comparing(GroceryItem::getPrice))
-                .collect(Collectors.toList()).reversed().subList(0, 3);
+        return groceryItems.stream().sorted(Comparator.comparing(GroceryItem::getPrice)).collect(Collectors.toList()).reversed().subList(0, 3);
     }
+
     public List<String> top3Names() {
-        return sortByPrice2().stream()
-                .map(GroceryItem::getName)
-                .collect(Collectors.toList());
+        return sortByPrice2().stream().map(GroceryItem::getName).collect(Collectors.toList());
     }
+
     public void printWithDiscount() {
-        groceryItems
-                .forEach(groceryItem -> System.out.println(groceryItem.getName()+": "+ String.format("%.2f", groceryItem.getPrice()*0.9)+" Br"));
-    }
-    public boolean findLessOne(){
-       return groceryItems.stream()
-                .anyMatch(groceryItem -> groceryItem.getPrice()<1);
-    }
-    public boolean allMoreZero(){
-        return groceryItems.stream()
-                .allMatch(groceryItem -> groceryItem.getPrice()>0);
-    }
-    public GroceryItem findFirstVegetable(){
-       Optional<GroceryItem> groceryItem = groceryItems.stream()
-               .filter(groceryItem1 -> groceryItem1.getCategory() == Category.VEGETABLE)
-               .findFirst();
-       return groceryItem.get();
-    }
-    public  List<Category> getAllCategory(){
-        return groceryItems.stream()
-                .map(GroceryItem::getCategory)
-                .distinct()
-                .collect(Collectors.toList());
+        groceryItems.forEach(groceryItem -> System.out.println(groceryItem.getName() + ": " + String.format("%.2f", groceryItem.getPrice() * 0.9) + " Br"));
     }
 
+    public boolean findLessOne() {
+        return groceryItems.stream().anyMatch(groceryItem -> groceryItem.getPrice() < 1);
+    }
 
+    public boolean allMoreZero() {
+        return groceryItems.stream().allMatch(groceryItem -> groceryItem.getPrice() > 0);
+    }
+
+    public GroceryItem findFirstVegetable() {
+        Optional<GroceryItem> groceryItem = groceryItems.stream().filter(groceryItem1 -> groceryItem1.getCategory() == Category.VEGETABLE).findFirst();
+        return groceryItem.get();
+    }
+
+    public List<Category> getAllCategory() {
+        return groceryItems.stream().map(GroceryItem::getCategory).distinct().collect(Collectors.toList());
+    }
 
 
     public List<GroceryItem> getGroceryItems() {
